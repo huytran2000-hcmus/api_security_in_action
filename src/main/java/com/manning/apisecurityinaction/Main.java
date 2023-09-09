4330193c3886cf160dd637a31b378669a349eb9b
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2023-09-10 01:16:54 +0700

    Prototype the natter api

---
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -0,0 +1,73 @@
+package com.manning.apisecurityinaction;
+
+import static spark.Spark.afterAfter;
+import static spark.Spark.before;
+import static spark.Spark.exception;
+import static spark.Spark.halt;
+import static spark.Spark.internalServerError;
+import static spark.Spark.notFound;
+import static spark.Spark.post;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.dalesbred.Database;
+import org.dalesbred.result.EmptyResultException;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.manning.apisecurityinaction.controller.SpaceController;
+
+import spark.Request;
+import spark.Response;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        var datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter", "password");
+        var database = Database.forDataSource(datasource);
+        createTables(database);
+
+        datasource = JdbcConnectionPool.create("jdbc:h2:mem:natter", "natter_api_user", "password");
+        database = Database.forDataSource(datasource);
+
+        before((request, response) -> {
+            if (request.requestMethod().equals("POST") && !request.contentType().equals("application/json")) {
+                halt(415, new JSONObject().put("error", "Only support application/json").toString());
+            }
+        });
+
+        var spaceController = new SpaceController(database);
+        post("/spaces", spaceController::createSpace);
+
+        afterAfter((request, response) -> {
+            response.header("Server", "");
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nonsniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("Cache-Control", "no-store");
+            response.header("X-XSS-Protection", "0");
+            response.header("Content-Security-Policy", "default-src 'none'; frame-ancestors 'none'; sandbox");
+        });
+
+        internalServerError(new JSONObject().put("error", "internal server error").toString());
+        notFound(new JSONObject().put("error", "not found").toString());
+
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class,
+                Main::badRequest);
+        exception(EmptyResultException.class,
+                (e, request, response) -> response.status(404));
+    }
+
+    private static void badRequest(Exception ex, Request request, Response response) {
+        response.status(400);
+        response.body(new JSONObject()
+                .put("error", ex.getMessage()).toString());
+    }
+
+    private static void createTables(Database database) throws Exception {
+        var path = Paths.get(Main.class.getResource("/schema.sql").toURI());
+        database.update(Files.readString(path));
+    }
+}
---
+++ src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -0,0 +1,44 @@
+package com.manning.apisecurityinaction.controller;
+
+import java.sql.SQLException;
+
+import org.dalesbred.Database;
+import org.json.JSONObject;
+
+import spark.Request;
+import spark.Response;
+
+public class SpaceController {
+    private final Database database;
+
+    public SpaceController(Database database) {
+        this.database = database;
+    }
+
+    public JSONObject createSpace(Request request, Response response) throws SQLException {
+        var json = new JSONObject(request.body());
+        var spaceName = json.getString("name");
+        if (spaceName.length() > 255) {
+            throw new IllegalArgumentException("space name is too long");
+        }
+
+        var owner = json.getString("owner");
+        if (!owner.matches("[a-zA-Z][a-zA-Z0-9]{1,29}")) {
+            throw new IllegalArgumentException("invalid username");
+        }
+
+        return database.withTransaction(tx -> {
+            var spaceId = database.findUniqueLong("SELECT NEXT VALUE FOR space_id_seq");
+            // database.updateUnique("INSERT INTO spaces (space_id, name, owner) "
+            // + "VALUES(?, ?, ?);", spaceId, spaceName, owner);
+            database.updateUnique("INSERT INTO spaces (space_id, name, owner) VALUES(" + spaceId + ", '" + spaceName
+                    + "', '" + owner + "');");
+
+            response.status(201);
+            var uri = "/spaces/" + spaceId;
+            response.header("Location", uri);
+
+            return new JSONObject().put("name", spaceName).put("uri", uri).put("owner", owner);
+        });
+    }
+}
